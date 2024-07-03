786633f814fdd0b8d70826f7f4f13348c909acc0
kayke <kaykeharmann3000@gmail.com>
2024-07-03 11:02:45 -0300

    projeto

--- calc.js
+++ calc.js
@@ -10,37 +10,35 @@ function menu(){
         "\n3- multiplicação"+
         "\n4- divisao"+
         "\n5- porcentagem");
-       
-   }
+   
+}
 let opcao = prompt("digite a operação: ");
 let numero1 = prompt("digite o primeiro valor: ");
 let numero2 = prompt("digite o segundo valor: ");
-numero1 = Number(numero1);
-numero2 = Number(numero2);
-   if(isNaN(numero1) || isNaN(numero2)){
-       console.log("digite um valor valido: ");
-   }
-    switch(opcao){
-        case '1':
-            
-          console.log("O resultado da operação e: " + (numero1 + numero2));
-            break;
-        case '2':
-            
-        console.log("O resultado da operação e: " + (numero1 - numero2));
-            break;
-        case '3':
-           
-        console.log("O resultado da operação e: " + (numero1 * numero2));
-            break;
-        case '4':
-            
-        console.log("O resultado da operação e: " + (numero1 / numero2));
-            break;
-        case '5':
-        
-            console.log("O resultado da operação e: " + (numero1/100) *  numero2);
-            break;
-       
-    }
-    return;
+
+    numero1 = Number(numero1);
+    numero2 = Number(numero2);
+
+
+if(isNaN(numero1) || isNaN(numero2)){
+    console.log("digite um valor valido: ");
+}
+
+switch(opcao){
+    case '1':
+        console.log("O resultado da operação e: " + (numero1 + numero2));
+        break;
+    case '2':
+    console.log("O resultado da operação e: " + (numero1 - numero2));
+        break;
+    case '3':
+    console.log("O resultado da operação e: " + (numero1 * numero2));
+        break;
+    case '4':    
+    console.log("O resultado da operação e: " + (numero1 / numero2));
+        break;
+    case '5':
+        console.log("O resultado da operação e: " + (numero1/100) *  numero2);
+        break;
+}
+return;
